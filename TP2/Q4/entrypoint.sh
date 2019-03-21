8d462f6702e90214445e561cb234df908c825b59
DjalilMalti <malti.abdeljelil@gmail.com>
2019-03-21 19:56:55 -0400

    L'image de docker avec Jmeter est faite. Il reste des configurations a ajouteret a arranger pour que Jmeter fasse exactement les testes qu'on veut qu'il fasse. A ce point il s'agit surtout de savoir comment utiliser jmeter

--- TP2/Q4/entrypoint.sh
+++ TP2/Q4/entrypoint.sh
@@ -10,19 +10,27 @@ s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
-
+export TEST_DIR="/tests"
+export TEST_PLAN="test_jmeter_weka"
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
 echo "jmeter args=$@"
 
+pwd
+
+ls -al
+
+ls -al /tests
+
 # Keep entrypoint simple: we must pass the standard JMeter arguments
-jmeter $@
+jmeter -n -t "${TEST_DIR}/${TEST_PLAN}.jmx" \
+   -l "${TEST_DIR}/${TEST_PLAN}.jtl" $@
 echo "END Running Jmeter on `date`"
 
 #     -n \
 #    -t "/tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
 #    -l "/tests/${TEST_DIR}/${TEST_PLAN}.jtl"
-# exec tail -f jmeter.log
+exec tail -f ${TEST_DIR}/jmeter.log
 #    -D "java.rmi.server.hostname=${IP}" \
 #    -D "client.rmi.localport=${RMI_PORT}" \
 #  -R $REMOTE_HOSTS
